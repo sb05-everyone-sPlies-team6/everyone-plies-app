096b0d46d9ef281b9c6b93e773a2006bc766c9f3
Seoyeon-Jeong <seoyeoni945@gmail.com>
2026-01-02 14:38:26 +0900

    feat: 사용자 조회 구현

--- src/main/java/team6/finalproject/domain/user/controller/UserController.java
+++ src/main/java/team6/finalproject/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,11 @@ public class UserController {
   public ResponseEntity<User> create(@RequestBody @Valid UserCreateRequest request) {
     User user = userService.create(request);
     return ResponseEntity.status(HttpStatus.CREATED).body(user);
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<User> findById(@PathVariable Long userId) {
+    User user = userService.findById(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(user);
   }
 }
--- src/main/java/team6/finalproject/domain/user/service/UserService.java
+++ src/main/java/team6/finalproject/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package team6.finalproject.domain.user.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import team6.finalproject.domain.user.dto.UserCreateRequest;
 import team6.finalproject.domain.user.entity.User;
 import team6.finalproject.domain.user.repository.UserRepository;
@@ -23,5 +26,14 @@ public class UserService {
     User user = new User(request.email(), request.password(), request.name());
 
     return userRepository.save(user);
+  }
+
+  @Transactional(readOnly = true)
+  public User findById(Long userId) {
+
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+    return user;
   }
 }
