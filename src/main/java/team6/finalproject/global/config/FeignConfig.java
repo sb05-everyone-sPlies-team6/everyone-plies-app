1c6267b1240b6b1b2a72c86e4677b29fc45acf35
yunji1014 <7787yunji@gmail.com>
2026-01-09 11:17:02 +0900

    안쓰는 클래스 삭제

--- src/main/java/team6/finalproject/global/config/FeignConfig.java
+++
@@ -1,9 +0,0 @@
-package team6.finalproject.global.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "team6.finalproject.domain.content.api")
-public class FeignConfig {
-}
