0ba61ecc6938532d318d5a6985d7f5bc2c50a137
yunji1014 <7787yunji@gmail.com>
2026-01-15 01:02:14 +0900

    fix/DM response 프론트 명세와 맞춤

--- src/main/java/team6/finalproject/domain/dm/dto/DmResponse.java
+++ src/main/java/team6/finalproject/domain/dm/dto/DmResponse.java
@@ -1,8 +1,10 @@
 package team6.finalproject.domain.dm.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record DmResponse(
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	Long id,
 	UserSimpleResponse with,
 	@JsonProperty("lastestMessage")
--- src/main/java/team6/finalproject/domain/dm/dto/MessageResponse.java
+++ src/main/java/team6/finalproject/domain/dm/dto/MessageResponse.java
@@ -2,8 +2,11 @@ package team6.finalproject.domain.dm.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public record MessageResponse(
 	Long id,
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	Long conversationId,
 	LocalDateTime createdAt,
 	UserSimpleResponse sender,
