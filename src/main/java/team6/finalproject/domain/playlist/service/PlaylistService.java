fb18b9663cdd4a6812e8b73cc27fbe3fbfa87992
Seoyeon-Jeong <seoyeoni945@gmail.com>
2026-01-19 18:27:00 +0900

    feat: 플레이리스트 구독 알림 구현

--- src/main/java/team6/finalproject/domain/playlist/service/PlaylistService.java
+++ src/main/java/team6/finalproject/domain/playlist/service/PlaylistService.java
@@ -1,10 +1,18 @@
 package team6.finalproject.domain.playlist.service;
 
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team6.finalproject.domain.content.dto.ContentSummary;
 import team6.finalproject.domain.content.repository.ContentRepository;
+import team6.finalproject.domain.notification.dto.NotificationDto;
+import team6.finalproject.domain.notification.entity.Level;
+import team6.finalproject.domain.notification.entity.Notification;
+import team6.finalproject.domain.notification.entity.TargetType;
+import team6.finalproject.domain.notification.repository.NotificationRepository;
 import team6.finalproject.domain.playlist.dto.CursorResponsePlaylistDto;
 import team6.finalproject.domain.playlist.dto.PlaylistDto;
 import team6.finalproject.domain.playlist.dto.PlaylistCreateRequest;
@@ -15,6 +23,7 @@ import team6.finalproject.domain.playlist.entity.PlaylistSubscription;
 import team6.finalproject.domain.playlist.repository.PlaylistContentRepository;
 import team6.finalproject.domain.playlist.repository.PlaylistRepository;
 import team6.finalproject.domain.playlist.repository.PlaylistSubscriptionRepository;
+import team6.finalproject.domain.user.entity.User;
 import team6.finalproject.domain.user.repository.UserRepository;
 import team6.finalproject.domain.user.dto.UserSummary;
 
@@ -32,6 +41,8 @@ public class PlaylistService {
     private final PlaylistContentRepository playlistContentRepository;
     private final ContentRepository contentRepository;
     private final PlaylistSubscriptionRepository playlistSubscriptionRepository;
+    private final NotificationRepository notificationRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public PlaylistDto createPlaylist(Long userId, PlaylistCreateRequest request) {
@@ -200,6 +211,22 @@ public class PlaylistService {
         Playlist playlist = playlistRepository.findById(playlistId)
                 .orElseThrow(() -> new IllegalArgumentException("플레이리스트를 찾을 수 없습니다."));
         playlist.setTotalSubscription(playlist.getTotalSubscription() + 1);
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("User not found."));
+
+        Notification notification = new Notification(
+            playlist.getOwner(),
+            "PLAYLIST SUBSCRIBE",
+            user.getName() + "님이 회원님의 플레이리스트를 구독했습니다.",
+            Level.INFO,
+            playlist.getOwner().getId(),
+            TargetType.PLAYLIST_SUBSCRIBED
+        );
+
+        notificationRepository.save(notification);
+        NotificationDto saved = NotificationDto.from(notification);
+        eventPublisher.publishEvent(saved);
     }
 
     @Transactional
