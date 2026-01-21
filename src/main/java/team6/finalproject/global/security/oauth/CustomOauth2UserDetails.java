416fbeede24dd5e8a437c3c71238f30f81d1054f
Seoyeon-Jeong <seoyeoni945@gmail.com>
2026-01-21 14:10:04 +0900

    fix: OAuth2 로그인 권한 처리 중 누락된 List import 추가

--- src/main/java/team6/finalproject/global/security/oauth/CustomOauth2UserDetails.java
+++ src/main/java/team6/finalproject/global/security/oauth/CustomOauth2UserDetails.java
@@ -2,6 +2,7 @@ package team6.finalproject.global.security.oauth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
