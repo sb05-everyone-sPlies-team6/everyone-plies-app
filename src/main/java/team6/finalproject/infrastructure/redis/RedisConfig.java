a41d99917263ad514014a25b440ecbc76b69b76f
redmatoda <redmatoda@naver.com>
2026-01-26 14:08:58 +0900

    fix : redis 관련 환경변수 배포 테스트 및 RedisConfig 수정

--- src/main/java/team6/finalproject/infrastructure/redis/RedisConfig.java
+++ src/main/java/team6/finalproject/infrastructure/redis/RedisConfig.java
@@ -1,20 +1,32 @@
 package team6.finalproject.infrastructure.redis;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+@Configuration
 public class RedisConfig {
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
-  }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Elasticache Cluster 엔드포인트
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+        clusterConfig.clusterNode("clustercfg.redis1.jvzvvs.apn2.cache.amazonaws.com", 6379);
 
-  @Bean
-  public StringRedisTemplate stringRedisTemplate(
-      RedisConnectionFactory connectionFactory) {
-    return new StringRedisTemplate(connectionFactory);
-  }
+        // SSL 적용만
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()  // TLS 활성화
+                .build();
+
+        return new LettuceConnectionFactory(clusterConfig, clientConfig);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
 }
