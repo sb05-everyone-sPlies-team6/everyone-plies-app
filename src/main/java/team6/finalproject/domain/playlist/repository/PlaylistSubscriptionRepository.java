0ee7fbbb654443ec7ee72f5e472da74b5f80759e
Seoyeon-Jeong <seoyeoni945@gmail.com>
2026-01-28 18:08:59 +0900

    fix: 구독자에게 알림이 전송되도록 대상 로직 수정

--- src/main/java/team6/finalproject/domain/playlist/repository/PlaylistSubscriptionRepository.java
+++ src/main/java/team6/finalproject/domain/playlist/repository/PlaylistSubscriptionRepository.java
@@ -1,10 +1,13 @@
 package team6.finalproject.domain.playlist.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team6.finalproject.domain.playlist.entity.PlaylistSubscription;
 
 import java.util.Optional;
+import team6.finalproject.domain.user.entity.User;
 
 public interface PlaylistSubscriptionRepository extends JpaRepository<PlaylistSubscription, Long> {
     Optional<PlaylistSubscription> findByUserIdAndPlaylistId(Long userId, Long playlistId);
+    List<Long> findUserIdsByPlaylistId(Long playlistId);
 }
